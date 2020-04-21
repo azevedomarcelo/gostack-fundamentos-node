04995226eba897499c4e8706ab4155192db836b9
marcelo.azevedo <marcelo.azevedo@connectcorp.com.br>
2020-04-21 13:43:13 -0300

    desafio gostack fundamentos node

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,39 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (accumulator: Balance, transactions: Transaction) => {
+        switch (transactions.type) {
+          case 'income':
+            accumulator.income += transactions.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += transactions.value;
+            break;
+          default:
+            break;
+        }
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transactions = new Transaction({ title, value, type });
+    this.transactions.push(transactions);
+    return transactions;
   }
 }
 
